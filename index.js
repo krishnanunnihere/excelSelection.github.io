ebc75537b0a1b85e3f93646afc19044b62e96156
krishnanunnihere <krishnanunnitk1999@gmail.com>
2019-03-13 20:48:17 +0530

    js added

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+
+function saveData()
+{
+  var data;
+  fetch('https://jsonplaceholder.typicode.com/posts',{
+    method:'POST',
+    headers:
+    {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res=>res.json())
+  .then(response=>console.log('SUCCESS',JSON.stringify(response)))
+  .catch(error=>comsole.error('ERROR:',error));
+}
+
 function showPass() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  } 
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+} 
